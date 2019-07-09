69def37d1c9340240b0f04d06ab3d5222283e653
Vincent Mwendwa <vrubben@outlook.com>
2019-07-09 16:05:22 +0300

    Added Amount Field, updated SendLibra view

--- ui/pages/ReceiveLibra.js
+++ ui/pages/ReceiveLibra.js
@@ -1,99 +1,131 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import {
-    Box, ListItemText, ListItem, Avatar, Icon, Button
+    Box, ListItemText, ListItem, Avatar, Icon, Button, Snackbar, IconButton
 } from "@material-ui/core"
 import QRCode from 'qrcode.react'
-import { ShareOutlined, ChevronLeftRounded } from "@material-ui/icons"
+import { ShareOutlined, ChevronLeftRounded, CloseOutlined } from "@material-ui/icons"
 import { makeStyles } from "@material-ui/styles"
+import LibraKePocWallet from "../LibraKePocWallet"
 
 const useStyles = makeStyles(theme => ({
     avatar: {
         backgroundColor: theme.palette.primary.main,
         color: '#fff',
         borderRadius: '50%'
+    },
+    primaryText: {
+        fontWeight: 500
+    },
+    secondaryText: {
+        fontWeight: 400,
+        fontSize: 11,
+        color: '#aaa'
+    },
+    listItem: {
+        padding: 0,
+        cursor: "pointer"
     }
 }))
 
 export default ({ history }) => {
 
+    const classes = useStyles()
+    const [isOpen, setIsOpen] = useState(false)
     const onBack = e => history.goBack()
+    const onCopy = address => {
+
+        navigator.clipboard.writeText(address).then(() => {
+            setIsOpen(true)
+        }, error => {
+            console.log(error)
+        })
+    }
+
     return (
-        <Fragment>
-            <Box width="90%" mt={2}>
-                <QRCode
-                    size={150}
-                    value="987092e9a29cc9579189d0bb38187e609216ed86ab7abd3a95025fad44f2e139" />
-            </Box>
-            <Box width="90%">
-                <ListItemText
-                    primaryTypographyProps={{
-                        variant: "body1",
-                        noWrap: true,
-                        style: {
-                            fontWeight: 500
-                        }
-                    }}
-                    secondaryTypographyProps={{
-                        variant: "body1",
-                        noWrap: true,
-                        style: {
-                            fontWeight: 400, fontSize: 11, color: '#aaa'
-                        }
-                    }}
-                    primary="My Account Address"
-                    secondary="987092e9a29cc9579189d0bb38187e609216ed86ab7abd3a95025fad44f2e139" />
-            </Box>
-            <Box width="90%">
-                <ListItem>
-                    <Avatar>
-                        <Icon fontSize="small" className="far fa-clone" />
-                    </Avatar>
-                    <ListItemText
-                        primaryTypographyProps={{
-                            variant: "body1",
-                            noWrap: true,
-                            style: {
-                                fontWeight: 500
-                            }
-                        }}
-                        secondaryTypographyProps={{
-                            variant: "body1",
-                            noWrap: true,
-                            style: {
-                                fontWeight: 400, fontSize: 11, color: '#aaa'
-                            }
-                        }}
-                        primary="Copy Address" secondary="Copy address to clipboard" />
-                </ListItem>
-            </Box>
-            <Box width="90%">
-                <ListItem>
-                    <Avatar>
-                        <ShareOutlined fontSize="small" />
-                    </Avatar>
-                    <ListItemText
-                        primaryTypographyProps={{
-                            variant: "body1",
-                            noWrap: true,
-                            style: {
-                                fontWeight: 500
-                            }
-                        }}
-                        secondaryTypographyProps={{
-                            variant: "body1",
-                            noWrap: true,
-                            style: {
-                                fontWeight: 400, fontSize: 11, color: '#aaa'
-                            }
-                        }}
-                        primary="Share account address" secondary="Copy address to clipboard" />
-                </ListItem>
-            </Box>
-            <Box mt={4} width="90%">
-                <Button onClick={onBack} variant="outlined" fullWidth size="small">
-                    <ChevronLeftRounded fontSize="small" /> Back
-                </Button>
-            </Box>
-        </Fragment>
+        <LibraKePocWallet.AccountContext.Consumer>
+            {({ account }) => (
+                <Fragment>
+                    <Snackbar
+                        anchorOrigin={{ vertical: 'top', horizontal: 'center', }}
+                        open={isOpen}
+                        autoHideDuration={3000}
+                        onClose={() => setIsOpen(false)}
+                        ContentProps={{ 'aria-describedby': 'message-id', }}
+                        message="Address copied successfully"
+                        action={[
+                            <IconButton
+                                key="close"
+                                aria-label="Close"
+                                color="inherit"
+                                onClick={() => setIsOpen(false)}>
+                                <CloseOutlined />
+                            </IconButton>]} />
+                    <Box width="90%" mt={2}>
+                        <QRCode
+                            size={150}
+                            value={account.address} />
+                    </Box>
+                    <Box width="90%">
+                        <ListItemText
+                            primaryTypographyProps={{
+                                variant: "body1",
+                                noWrap: true,
+                                className: classes.primaryText
+                            }}
+                            secondaryTypographyProps={{
+                                variant: "body1",
+                                noWrap: true,
+                                className: classes.secondaryText
+                            }}
+                            primary="My Account Address"
+                            secondary={account.address} />
+                    </Box>
+                    <Box width="90%">
+                        <ListItem className={classes.listItem} onClick={(e) => onCopy(account.address)}>
+                            <Avatar>
+                                <Icon fontSize="small" className="far fa-clone" />
+                            </Avatar>
+                            <ListItemText
+                                primaryTypographyProps={{
+                                    variant: "body1",
+                                    noWrap: true,
+                                    className: classes.primaryText
+                                }}
+                                secondaryTypographyProps={{
+                                    variant: "body1",
+                                    noWrap: true,
+                                    className: classes.secondaryText
+                                }}
+                                primary="Copy Address" secondary="Copy address to clipboard" />
+                        </ListItem>
+                    </Box>
+                    <Box width="90%">
+                        <ListItem className={classes.listItem}>
+                            <Avatar>
+                                <ShareOutlined fontSize="small" />
+                            </Avatar>
+                            <ListItemText
+                                primaryTypographyProps={{
+                                    variant: "body1",
+                                    noWrap: true,
+                                    className: classes.primaryText
+                                }}
+                                secondaryTypographyProps={{
+                                    variant: "body1",
+                                    noWrap: true,
+                                    className: classes.secondaryText
+                                }}
+                                primary="Share account address" secondary="Copy address to clipboard" />
+                        </ListItem>
+                    </Box>
+                    <Box mt={4} width="90%">
+                        <Button onClick={onBack} variant="outlined" fullWidth size="small">
+                            <ChevronLeftRounded fontSize="small" />
+                            {"Back"}
+                        </Button>
+                    </Box>
+                </Fragment>
+            )}
+        </LibraKePocWallet.AccountContext.Consumer>
     )
 }
